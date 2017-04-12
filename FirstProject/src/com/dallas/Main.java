4b7319fc0edb144882890668768ecf9f17d405d3
vinj-venkat <vinjanampati@gmail.com>
2017-04-12 00:28:03 -0500

    working version

--- FirstProject/src/com/dallas/Main.java
+++ FirstProject/src/com/dallas/Main.java
@@ -3,23 +3,109 @@ package com.dallas;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
-        String fileName = "D:\\HelloWorld\\src\\Input.txt";
+        // write your code here
+        String fileName = "D:\\Java_Projects\\FirstProject\\src\\Input.txt";
         ArrayList<String> lines = GetDataFromFile(fileName);
-        for (String item: lines
-             ) {
-            GetDataFromLine(item);
+
+        TreeMap<String, com.dallas.Node> nodeCollection = new TreeMap<String, com.dallas.Node>();
+
+        for (String item: lines)
+        {
+            List<String> lineItem = GetDataFromLine(item);
+            AddLineItem(lineItem, nodeCollection);
         }
-        System.out.print(lines);
+        //System.out.print(lines);
+
+        Display(nodeCollection);
+    }
+
+    private static void Display(TreeMap<String, Node> nodeCollection)
+    {
+        for(Map.Entry<String, Node> entry : nodeCollection.entrySet())
+        {
+            HashSet<String> visitedNodes = new HashSet<>();
+            visitedNodes.add(entry.getKey());
+            System.out.println(entry.getValue().getNodeName());
+            System.out.println("|");
+
+            for (String dependentNodeString : entry.getValue().getDependencyNodes())
+            {
+                if(nodeCollection.containsKey(dependentNodeString) && nodeCollection.get(dependentNodeString).getDependencyNodes().size() > 0) {
+                    DisplayNodeRecursive(dependentNodeString, nodeCollection, visitedNodes);
+                }
+            }
+        }
+    }
+
+    private static void DisplayNodeRecursive(String currentNode, TreeMap<String, com.dallas.Node> nodeCollection, HashSet<String> visitedNodes)
+    {
+        System.out.println("-" + currentNode);
+
+        if(visitedNodes.contains(currentNode))
+        {
+            return;
+        }
+
+
+
+        if(nodeCollection.containsKey(currentNode))
+        {
+            for (String dependentNodeString : nodeCollection.get(currentNode).getDependencyNodes())
+            {
+                System.out.print("-" + dependentNodeString);
+                System.out.println("|");
+
+                if(visitedNodes.contains(dependentNodeString))
+                {
+                    return;
+                }
+
+                visitedNodes.add((dependentNodeString));
+                if(nodeCollection.containsKey(dependentNodeString)) {
+                    com.dallas.Node dependantNode = nodeCollection.get(dependentNodeString);
+                    for(String newCurrentNode : dependantNode.getDependencyNodes())
+                    {
+                        DisplayNodeRecursive(newCurrentNode, nodeCollection, visitedNodes);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+    private static void AddLineItem(List<String> lineItem, TreeMap<String, com.dallas.Node> nodeCollection) {
+
+        String leftItem = lineItem.get(0).trim();
+        String rightItem = lineItem.get(1).trim();
+
+        if(nodeCollection.containsKey(leftItem))
+        {
+            Node currentNode = nodeCollection.get(leftItem);
+            currentNode.getDependencyNodes().add(rightItem);
+        }
+        else
+        {
+            Node node = new Node();
+            node.setNodeName(leftItem);
+            List<String> dependencyNodes = new ArrayList<>();
+            dependencyNodes.add(rightItem);
+            node.setDependencyNodes(dependencyNodes);
+            nodeCollection.put(leftItem, node);
+        }
     }
 
     private static ArrayList<String> GetDataFromFile(String fileName)
--- FirstProject/src/com/dallas/Node.java
+++ FirstProject/src/com/dallas/Node.java
@@ -7,6 +7,7 @@ import java.util.List;
  * Created by vinj-venkat on 4/2/2017.
  */
 public class Node {
+
     private String nodeName;
     private List<String> dependencyNodes = new ArrayList();
 
